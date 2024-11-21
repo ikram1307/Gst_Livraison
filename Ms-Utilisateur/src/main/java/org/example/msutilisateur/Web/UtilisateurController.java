9f62d16d81454f0e451b64a5e73a8209650362fa
ikram <samamiikram@gmail.com>
2024-11-21 17:42:02 +0100

    last commit

--- Ms-Utilisateur/src/main/java/org/example/msutilisateur/Web/UtilisateurController.java
+++ Ms-Utilisateur/src/main/java/org/example/msutilisateur/Web/UtilisateurController.java
@@ -8,38 +8,44 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/utilisateur")
 @RestController
+@RequestMapping("/utilisateurs") // Base URL for the controller
 public class UtilisateurController {
 
-        @Autowired
-        private UtilisateurService utilisateurService;
+    @Autowired
+    private UtilisateurService utilisateurService;
 
-        @PostMapping
-        public ResponseEntity<Utilisateur> createUtilisateur(@RequestBody Utilisateur utilisateur) {
-            return ResponseEntity.ok(utilisateurService.createUtilisateur(utilisateur));
-        }
+    // POST: Create a new Utilisateur
+    @PostMapping("/newuser")
+    public ResponseEntity<Utilisateur> createUtilisateur(@RequestBody Utilisateur utilisateur) {
+        return ResponseEntity.ok(utilisateurService.createUtilisateur(utilisateur));
+    }
 
-        @GetMapping("/listUser")
-        public ResponseEntity<List<Utilisateur>> getAllUtilisateurs() {
-            return ResponseEntity.ok(utilisateurService.getAllUtilisateurs());
-        }
+    // GET: Get all Utilisateurs
+    @GetMapping("/listUser")
+    public ResponseEntity<List<Utilisateur>> getAllUtilisateurs() {
+        return ResponseEntity.ok(utilisateurService.getAllUtilisateurs());
+    }
 
-        @GetMapping("/{id}")
-        public ResponseEntity<Utilisateur> getUtilisateurById(@PathVariable Long id) {
-            return utilisateurService.getUtilisateurById(id)
-                    .map(ResponseEntity::ok)
-                    .orElse(ResponseEntity.notFound().build());
-        }
+    // GET: Get a Utilisateur by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Utilisateur> getUtilisateurById(@PathVariable Long id) {
+        return utilisateurService.getUtilisateurById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-        @PutMapping("/{id}")
-        public ResponseEntity<Utilisateur> updateUtilisateur(@PathVariable Long id, @RequestBody Utilisateur updatedUtilisateur) {
-            return ResponseEntity.ok(utilisateurService.updateUtilisateur(id, updatedUtilisateur));
-        }
+    // PUT: Update a Utilisateur
+    @PutMapping("/{id}")
+    public ResponseEntity<Utilisateur> updateUtilisateur(@PathVariable Long id, @RequestBody Utilisateur updatedUtilisateur) {
+        return ResponseEntity.ok(utilisateurService.updateUtilisateur(id, updatedUtilisateur));
+    }
 
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Void> deleteUtilisateur(@PathVariable Long id) {
-            utilisateurService.deleteUtilisateur(id);
-            return ResponseEntity.noContent().build();
-        }
+    // DELETE: Delete a Utilisateur
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUtilisateur(@PathVariable Long id) {
+        utilisateurService.deleteUtilisateur(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
